5d54110eca9227fea6ce3b3683f7ec1e545d9383
Maya Ilieva <ilieva1maya@gmail.com>
2024-06-20 16:53:04 +0300

    Update Home/Carousel

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -6,11 +6,13 @@ export default function Carousel() {
                     <div className="carousel-item active">
                         <img className="w-100" src="img/forest1.png" alt="Forest" />
                         <div className="carousel-caption top-0 bottom-0 start-0 end-0 d-flex flex-column align-items-center justify-content-center">
-                            <div className="text-start p-5" style={{ maxWidth: 900 }}>
-                                <h3 className="text-white">Organic Vegetables</h3>
-                                <h1 className="display-1 text-white mb-md-4">Organic Vegetables For Healthy Life</h1>
-                                <a href="" className="btn btn-primary py-md-3 px-md-5 me-3">Explore</a>
-                                <a href="" className="btn btn-secondary py-md-3 px-md-5">Contact</a>
+                            <div className="text-start p-5" style={{ maxWidth: 1200 }}> 
+                            <h1 className="display-4 text-white mb-md-4">The Nature is beauty, the Nature is life!
+</h1>                               
+                                <h3 className="display-6 text-white mb-md-4">Want to plant a tree, but you never have time to?</h3>
+                                <h3 className="display-6 text-white mb-md-4">With just one click buy a tree from our Catalog, and we will plant it for you!</h3>
+                                
+                                <a href="" className="btn btn-secondary py-md-3 px-md-5">Catalog</a>
                             </div>
                         </div>
                     </div>
