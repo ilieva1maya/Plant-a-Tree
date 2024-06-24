d714a434fbc905b5d1bb9aad4583dd42c354ec31
Maya Ilieva <ilieva1maya@gmail.com>
2024-06-24 12:04:25 +0300

    Login and Logout request to the server

---
+++ client/src/lib/request.js
@@ -0,0 +1,40 @@
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers = {
+            'content-type': 'application/json'
+        }
+    }
+
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+        options.headers = {
+            ...options.headers,
+            'X-Authorization': token
+        }
+    }
+
+    return options;
+}
+
+export const request = async (method, url, data) => {
+    const response = await fetch(url, {        
+        ...buildOptions(data),
+        method,
+    });
+
+    if (response.status === 204) {
+        return {};
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
+   
+    return result;
+}
---
+++ client/src/services/authService.js
@@ -0,0 +1,28 @@
+import { request } from "../lib/request";
+import Path from "../paths";
+
+const baseUrl = 'http://localhost:3030/users';
+
+
+
+export const login = async (email, password) => {
+    const result = await request('POST', `${baseUrl}${Path.Login}`, {
+        email,
+        password,
+    });
+
+    return result;
+}
+
+// export const register = async (email, password) => {
+//     const result = await request('POST', `${baseUrl}${Path.Register}`, {
+//         email,
+//         password,
+//     });
+
+//     return result;
+// }
+
+export const logout = async () => {
+    await request('GET', `${baseUrl}${Path.Logout}`);
+}
