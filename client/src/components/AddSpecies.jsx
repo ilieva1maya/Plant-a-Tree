2c4dec9bf5a1f39d42cd729d23bf0974290ad1e8
Maya Ilieva <ilieva1maya@gmail.com>
2024-07-04 11:02:22 +0300

    Formatting

--- client/src/components/AddSpecies.jsx
+++ client/src/components/AddSpecies.jsx
@@ -1,52 +1,38 @@
 export default function AddSpecies() {
     return (
-        <div className="container-login100">
+
+        <div id="create-page" className="auth">
             <form>
-                <div >
+                <div className="container-add">
+
                     <h3>In botany, a tree is a perennial plant with an elongated stem, or trunk. Usually supporting branches and leaves. </h3>
 
-                    <div className="container-login100">
-                        <label htmlFor="name" className="txt4">Species Name:</label>
-                        <input className="input50" type="text" name="name" placeholder="Name" />
-                    </div>
+                    <label htmlFor="name" className="txt4">Species Name:</label>
+                    <input className="input100" type="text" name="name" placeholder="Name" />
 
-                    <div className="container-login100">
-                        <label htmlFor="family" className="txt4">Family:</label>
-                        <input className="input50" type="text" name="family" placeholder="Family" />
-                    </div>
+                    <label htmlFor="family" className="txt4">Family:</label>
+                    <input className="input100" type="text" name="family" placeholder="Family" />
 
-                    <div className="container-login100">
-                        <label htmlFor="height" className="txt4">Height:</label>
-                        <input className="input50" type="text" name="height" placeholder="Height" />
-                    </div>
+                    <label htmlFor="height" className="txt4">Height:</label>
+                    <input className="input100" type="text" name="height" placeholder="Height" />
 
-                    <div className="container-login100">
-                        <label htmlFor="exposure" className="txt4">Exposure:</label>
-                        <input className="input50" type="text" name="exposure" placeholder="Exposure" />
-                    </div>
+                    <label htmlFor="exposure" className="txt4">Exposure:</label>
+                    <input className="input100" type="text" name="exposure" placeholder="Exposure" />
 
-                    <div className="container-login100">
-                        <label htmlFor="soil" className="txt4">Soil:</label>
-                        <input className="input50" type="text" name="soil" placeholder="Soil" />
-                    </div>
+                    <label htmlFor="soil" className="txt4">Soil:</label>
+                    <input className="input100" type="text" name="soil" placeholder="Soil" />
 
-                    <div className="container-login100">
-                        <label htmlFor="foliage" className="txt4">Foliage:</label>
-                        <input className="input50" type="text" name="foliage" placeholder="Foliage" />
-                    </div>
+                    <label htmlFor="foliage" className="txt4">Foliage:</label>
+                    <input className="input100" type="text" name="foliage" placeholder="Foliage" />
 
-                    <div className="container-login100">
-                        <label htmlFor="more" className="txt4">More:</label>
-                        <input className="input50" type="text" name="more" placeholder="Tell us something interesting..." />
-                    </div>
+                    <label htmlFor="more" className="txt4">More:</label>
+                    <input className="input100" type="text" name="more" placeholder="Tell us something interesting..." />
 
-                    <div className="container-login100-form-btn">
-                        <button className="login50-form-btn">
-                            Add Species
-                        </button>
-                    </div>
+                    <button className="login100-form-btn">
+                        Add Species
+                    </button>
+
                 </div>
-
             </form>
         </div>
     )
