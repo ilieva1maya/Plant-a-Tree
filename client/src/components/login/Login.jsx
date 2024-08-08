ac35ff87c9f81c524fc801094e379aa00f20bb91
Maya Ilieva <ilieva1maya@gmail.com>
2024-08-08 16:16:10 +0300

    Controlled Login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,26 +1,44 @@
+import { useContext } from "react";
+import useForm from "../../hooks/useForm";
+import AuthContext from "../../contexts/authContext";
+
+const LoginFormKeys = {
+	Email: 'email',
+	Password: 'password'
+}
+
 export default function Login() {
-    return(
-        
+	const { loginSubmitHandler } = useContext(AuthContext);
+	const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+		[LoginFormKeys.Email]: '',
+		[LoginFormKeys.Password]: '',
+	});
+	return (
+
 		<div className="container-login100">
 			<div className="wrap-login100">
-				<form className="login100-form validate-form p-l-55 p-r-55 p-t-150">
+				<form className="login100-form validate-form p-l-55 p-r-55 p-t-150" onSubmit={onSubmit}>
 					<span className="login100-form-title">
 						Sign In
 					</span>
 
 					<div className="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
-						<input className="input100" type="text" name="username" placeholder="E-mail"/>
+						<input className="input100" type="text" name={LoginFormKeys.Email} placeholder="E-mail"
+							onChange={onChange}
+							value={values[LoginFormKeys.Email]} />
 						<span className="focus-input100"></span>
 					</div>
 
-					<div className="wrap-input100 validate-input" data-validate = "Please enter password">
-						<input className="input100" type="password" name="pass" placeholder="Password"/>
+					<div className="wrap-input100 validate-input" data-validate="Please enter password">
+						<input className="input100" type="password" name={LoginFormKeys.Password} placeholder="Password"
+							onChange={onChange}
+							value={values[LoginFormKeys.Password]} />
 						<span className="focus-input100"></span>
 					</div>
 
 					<div className="text-right p-t-13 p-b-23">
 						<a href="#" className="txt2">
-						Forgot Username / Password?
+							Forgot Username / Password?
 						</a>
 					</div>
 
@@ -42,6 +60,6 @@ export default function Login() {
 				</form>
 			</div>
 		</div>
-	
-    )
+
+	)
 }
