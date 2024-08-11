b302b091b14b74d0b848f80f95e5a6d73f4e84b6
Maya Ilieva <ilieva1maya@gmail.com>
2024-08-11 23:10:31 +0300

    Fine tunning

--- client/src/components/contacts/ContacUs.jsx
+++ client/src/components/contacts/ContacUs.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Path from "../../paths";
 
 export default function ContactUs() {
 
@@ -7,7 +8,7 @@ export default function ContactUs() {
         e.preventDefault();
 
         alert('Thanks for contacting us! We will come back to you shortly!');
-        navigate('/');
+        navigate(Path.Home);
     }
 
     return (
