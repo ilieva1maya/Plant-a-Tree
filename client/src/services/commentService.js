1dce2b6dffd2f3939e7225a0b0a9ce8fd7ee738f
Maya Ilieva <ilieva1maya@gmail.com>
2024-08-13 17:47:36 +0300

    Migration to collection commentService

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://localhost:3030/data/comments'
 
 export const create = async (id, username, text) => {
     const newComment = await request('POST', baseUrl, {
-        id,        
+        id,
         text,
     });
 
@@ -12,18 +12,11 @@ export const create = async (id, username, text) => {
 }
 
 export const getAll = async (id) => {
-    // This is the better solution, but it will works after migration to collection service
-    // const query = new URLSearchParams({
-    //     where: `id="${id}"`,
-    //     load: `owner=_ownerId:users`
-    // });
+    const query = new URLSearchParams({
+        where: `id="${id}"`,
+        load: `owner=_ownerId:users`
+    });
 
-    // const result = await request('GET', `${baseUrl}?${query}`);
-    // return result;
-
-    // Temp solution преи миграцията към collections
-    const result = await request('GET', baseUrl);
-
-    // преди миграцията към collections, понеже връща обект
-    return Object.values(result).filter(comment => comment.id === id);
+    const result = await request('GET', `${baseUrl}?${query}`);
+    return result;
 }
