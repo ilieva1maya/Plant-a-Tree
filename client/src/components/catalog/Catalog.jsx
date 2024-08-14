cc0970789dfd6fbe0cd8bf712b63f2f36714cc43
Maya Ilieva <ilieva1maya@gmail.com>
2024-08-14 16:49:12 +0300

    Update Catalog

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -10,17 +10,18 @@ export default function Catalog() {
 
     useEffect(() => {
         speciesService.getAll()
-            .then(result => setSpecies(result));
+            .then(result => setSpecies(result))
+            .catch(error => {
+                console.log(error)
+            });
     }, []);
 
     return (
         <div className="container-fluid py-5">
             <div className="container">
-                {/* <div className="owl-carousel product-carousel px-5"> */}
                 <div>
-
                     {species.length > 0 && (
-                        <div className="mx-auto text-center mb-5" style={{ maxWidth: 800 }}>                            
+                        <div className="mx-auto text-center mb-5" style={{ maxWidth: 800 }}>
                             <h3><i className="fa fa-arrow-down"></i>  Please choose from the <span className="text-primary text-uppercase">trees</span> listed below  <i className="fa fa-arrow-down"></i></h3>
                         </div>
                     )}
@@ -31,7 +32,7 @@ export default function Catalog() {
 
                     {species.length === 0 && (
                         <div className="mx-auto text-center mb-5" style={{ maxWidth: 800 }}>
-                            <h3 className="mb-5">Sorry, we do not have trees in our catalog. 
+                            <h3 className="mb-5">Sorry, we do not have trees in our catalog.
                                 Be the first to <Link to={Path.AddSpecies}><h3 className="text-primary text-uppercase">add</h3></Link></h3>
                             <img className="w-100" src="img/TreeToAdd.jpg" alt="Forest" />
                         </div>
