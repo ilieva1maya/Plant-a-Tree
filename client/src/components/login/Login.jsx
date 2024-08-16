fd2b8aeb7ffbe7ccf549ad74d2f4943089bd9d7c
Maya Ilieva <ilieva1maya@gmail.com>
2024-08-16 09:42:41 +0300

    Login with Formik

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,48 +1,95 @@
 import { useContext } from "react";
-import useForm from "../../hooks/useForm";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from 'formik';
+
+// import useForm from "../../hooks/useForm";
 import AuthContext from "../../contexts/authContext";
 import Path from "../../paths";
-import { Link, useNavigate } from "react-router-dom";
 
-const LoginFormKeys = {
-	Email: 'email',
-	Password: 'password'
-}
+// const LoginFormKeys = {
+// 	Email: 'email',
+// 	Password: 'password'
+// }
 
 export default function Login() {
 	const { loginSubmitHandler } = useContext(AuthContext);
-	const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
-		[LoginFormKeys.Email]: '',
-		[LoginFormKeys.Password]: '',
-	});
+	// const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+	// 	[LoginFormKeys.Email]: '',
+	// 	[LoginFormKeys.Password]: '',
+	// });
 
 	const navigate = useNavigate();
+
+	const formik = useFormik({
+		initialValues: {
+			email: '', 
+			password: '',
+			// confirmPassword: ''
+		},
+		onSubmit: (values) => {loginSubmitHandler(values)},
+		
+		
+		// validate: ({email, password, confirmPassword})=> {
+		// 	let errors = { email: '', password: ''};
+		// 	if(!email) {
+		// 		errors = {...errors, email: 'Email is required!'};				
+		// 	} else if (!/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/.test(email)) {
+		// 		errors = {...errors, email: 'Email is not valid!'};
+		// 	}
+		// 	if(!password) {
+		// 		errors = {...errors, password: 'Password is required!'};
+		// 	} else if (password.length < 6) {
+		// 		errors = {...errors, password: 'Password should be at least 6 characters!'};
+		// 	}
+		// 	if(!confirmPassword) {
+		// 		errors = {...errors, confirmPassword: 'Please confirm password!'};
+		// 	} else if (confirmPassword != password) {
+		// 		errors = {...errors, confirmPassword: 'Passwords do not match!'};
+		// 	}
+		// 	return errors;
+		// }
+	});
+
 	const forgottenPassword = async (e) => {
         e.preventDefault();
 
         prompt('Please type your email \nPlease follow the steps in your email for password recovery');
         navigate(Path.Home);
     }
+	
 
 	return (
 		<div className="container-login100">
 			<div className="wrap-login100">
-				<form className="login100-form validate-form p-l-55 p-r-55 p-t-150" onSubmit={onSubmit}>
+				<form className="login100-form validate-form p-l-55 p-r-55 p-t-150" onSubmit={formik.handleSubmit}>
 					<span className="login100-form-title">
 						Login
 					</span>
 
 					<div className="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
-						<input className="input100" type="text" name={LoginFormKeys.Email} placeholder="E-mail"
-							onChange={onChange}
-							value={values[LoginFormKeys.Email]} />
+						<input className="input100"  
+						id="email"
+						name="email" 
+						type="email"
+						placeholder="E-mail"
+						onChange={formik.handleChange}
+						value={formik.values.email} 
+						/>
+						{formik.errors.email && <p>{formik.errors.email}</p>}
 						<span className="focus-input100"></span>
 					</div>
-
+					
 					<div className="wrap-input100 validate-input" data-validate="Please enter password">
-						<input className="input100" type="password" name={LoginFormKeys.Password} placeholder="Password"
-							onChange={onChange}
-							value={values[LoginFormKeys.Password]} />
+						<input className="input100" 
+						id="password"
+						type="password" 
+						name="password"
+						placeholder="Password"
+						onChange={formik.handleChange}
+						value={formik.values.password} 
+						/>
+						{formik.errors.password && <p>{formik.errors.password}</p>}
 						<span className="focus-input100"></span>
 					</div>
 
@@ -52,8 +99,14 @@ export default function Login() {
 						</a>
 					</div>
 
-					<div className="container-login100-form-btn">
+					{/* <div className="container-login100-form-btn">
 						<button className="login100-form-btn">
+							Login
+						</button>
+					</div> */}
+
+					<div className="container-login100-form-btn">
+						<button type="submit" className="login100-form-btn">
 							Login
 						</button>
 					</div>
@@ -62,13 +115,10 @@ export default function Login() {
 						<span className="txt1 p-b-9">
 							Don’t have an account?
 						</span>
-
 						<Link to={Path.Register} className="txt3">Register now</Link>
-
 					</div>
 				</form>
 			</div>
 		</div>
-
 	)
 }
