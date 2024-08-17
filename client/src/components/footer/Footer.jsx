f66ae704df759b00248796bab28d56465b2c5788
Maya Ilieva <ilieva1maya@gmail.com>
2024-08-17 19:46:53 +0300

    Link to Firebase updated

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <>
             <div className="container-fluid bg-dark text-white py-4">
                 <div className="container text-center">
-                    <p className="mb-0">&copy; <a className="text-secondary fw-bold" href="#">Plant a Tree</a>. All Rights Reserved. Designed by <a className="text-secondary fw-bold" href="https://github.com/ilieva1maya">Maya Ilieva</a></p>
+                    <p className="mb-0">&copy; <a className="text-secondary fw-bold" href="https://plant-a-tree-6217b.web.app">Plant a Tree</a>. All Rights Reserved. Designed by <a className="text-secondary fw-bold" href="https://github.com/ilieva1maya">Maya Ilieva</a></p>
                 </div>
             </div>
         </>
